65036736bc2453f5d4acef2b27df6afcd6de7468
Escaper App <escaperapp@proton.me>
2026-03-12 10:43:05 +0300

    Add settings screen

--- composeApp/src/commonMain/kotlin/io/escaper/escaperapp/App.kt
+++ composeApp/src/commonMain/kotlin/io/escaper/escaperapp/App.kt
@@ -1,23 +1,8 @@
 package io.escaper.escaperapp
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -25,11 +10,9 @@ import androidx.navigation.compose.rememberNavController
 import io.escaper.escaperapp.navigation.EscaperScreen
 import io.escaper.escaperapp.navigation.ProvideNavController
 import io.escaper.escaperapp.platform.UpdateWindowBackground
-import io.escaper.escaperapp.presentation.mainscreen.MainScreenViewModel
 import io.escaper.escaperapp.presentation.common.EscaperTheme
-import io.escaper.escaperapp.presentation.components.dropdown.EscaperDropdown
-import io.escaper.escaperapp.presentation.components.mainbutton.OnOffButton
 import io.escaper.escaperapp.presentation.mainscreen.MainScreen
+import io.escaper.escaperapp.presentation.mainscreen.MainScreenViewModel
 import io.escaper.escaperapp.presentation.settings.SettingsScreen
 import org.koin.compose.viewmodel.koinViewModel
 
