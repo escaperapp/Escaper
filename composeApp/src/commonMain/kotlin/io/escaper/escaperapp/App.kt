ba2d840ce12f2ee3c6d993f5cb993114f4770719
Escaper App <escaperapp@proton.me>
2026-04-01 00:04:52 +0300

    Create add strategy screen base

--- composeApp/src/commonMain/kotlin/io/escaper/escaperapp/App.kt
+++ composeApp/src/commonMain/kotlin/io/escaper/escaperapp/App.kt
@@ -6,7 +6,6 @@ import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
