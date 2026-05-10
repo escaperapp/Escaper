8cb6a7d1b8677b1b1465522ae77b1a0a8af154f5
Escaper App <escaperapp@proton.me>
2026-05-10 19:17:09 +0300

    Implement argument input WIP

---
+++ composeApp/src/commonMain/kotlin/io/escaper/escaperapp/presentation/argsinput/ArgumentInputSelector.kt
@@ -0,0 +1,13 @@
+package io.escaper.escaperapp.presentation.argsinput
+
+import androidx.compose.runtime.Composable
+import io.escaper.escaperapp.domain.args.ArgumentKey
+
+@Composable
+internal fun ArgumentInputSelector(
+    isVisible: Boolean,
+    onSelect: (ArgumentKey) -> Unit,
+    onCancel: () -> Unit,
+) {
+
+}
