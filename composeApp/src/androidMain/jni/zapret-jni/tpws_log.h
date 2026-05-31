4d913af52b942c3ef185dd20253dd1d9b41817e8
Escaper App <escaperapp@proton.me>
2026-05-31 19:19:27 +0300

    Try debug zapret crash at startup

--- composeApp/src/androidMain/jni/zapret-jni/tpws_log.h
+++ composeApp/src/androidMain/jni/zapret-jni/tpws_log.h
@@ -2,7 +2,7 @@
 
 #include <android/log.h>
 
-#define LOG_TAG "TPWSJNI"
+#define LOG_TAG "ZAPRET"
 
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
